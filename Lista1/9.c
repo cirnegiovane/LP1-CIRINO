6a7dc788c7705385604765c9b5771c788e4c25ef
cirnegiovane <canalgiovane@hotmail.com>
2023-09-11 22:44:25 -0300

    Update 9.c

--- Lista1/9.c
+++ Lista1/9.c
@@ -1,2 +1,6 @@
 /*9)  Escrever um programa em C que calcule e imprima o número de caracteres distintos presentes em uma 
 cadeia de caracteres dada. Considerar o tamanho máximo da cadeia como sendo de 80 colunas.*/
+#include <stdio.h>
+int main(){
+    return 0;
+}
